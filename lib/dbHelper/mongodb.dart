d628ae7a40f3ca0bc8dec9a1cfe923d581587078
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2022-08-20 22:20:37 +0530

    first

---
+++ lib/dbHelper/mongodb.dart
@@ -0,0 +1,27 @@
+import 'dart:developer';
+import 'package:mongo_dart/mongo_dart.dart';
+import 'package:test1111/dbHelper/constant.dart';
+
+
+
+
+class MongoDatabase{
+  static var db, userCollection;
+  static connect() async{
+    db = await Db.create(MONGO_CONN_URL);
+    await db.open();
+    inspect(db);
+    userCollection = db.collection(USER_COLLECTION);
+
+  }
+
+  static Future<List<Map<String, dynamic>>> getData() async {
+      final arrData = await userCollection.find().toList();
+      return arrData;
+  }
+}
+
+
+
+
+
